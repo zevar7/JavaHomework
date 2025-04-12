b48198fbcc9e0fbed8318d802f73d2efb5e039a3
zevar7 <aminova.zevar@gmail.com>
2025-04-12 18:07:56 +0500

    Lesson 1

---
+++ src/main/java/Homework1/Lesson2.java
@@ -0,0 +1,67 @@
+package Homework1;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        byte b1 = 0b1100;
+        byte b2 = 014;
+        byte b3 = 12;
+        byte b4 = 0xC;
+
+        System.out.println((b1));
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+        short s1 = 0b10100010100;
+        short s2 = 02424;
+        short s3 = 1300;
+        short s4 = 0x514;
+
+        System.out.println( s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+        int i1 = 0b0;
+        int i2 = 00;
+        int i3 = 0;
+        int i4 = 0x0;
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        float f1 = 3.14F;
+        float f2 = 2.7172F;
+
+        System.out.println(f1);
+        System.out.println(f2);
+
+
+        double d1 = 0.123D;
+        double d2 = 9.8765;
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        boolean boo11 = false;
+        boolean boo12 =  true;
+
+        System.out.println(boo11);
+        System.out.println(boo12);
+
+        char c1 = 'z';
+        char c2 = '\u0555';
+        char c3 = 777;
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+        }
+
+
+ }
+
+
