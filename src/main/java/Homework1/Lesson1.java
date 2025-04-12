75d9b9f56f7d9060e0978721a041344e175f60a5
zevar7 <aminova.zevar@gmail.com>
2025-04-12 13:01:46 +0500

    Lesson 1

---
+++ src/main/java/Homework1/Lesson1.java
@@ -0,0 +1,12 @@
+package Homework1;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("         Rubai");
+        System.out.println("Mnogo let razmishlyal ya nad jiznyu zemnoi");
+        System.out.println("Neponyatnogo net dlya menya pod lunoi");
+        System.out.println("Mne izvestno, chto mne nichego ne izvestno");
+        System.out.println("Vot poslednaya pravda, otkritaya mnoi");
+        System.out.println("                                   O. Hayam");
+    }
+}
